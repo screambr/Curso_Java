f42d5b820154c38768e2e162ad4da133b796c29c
screambr <anderson.m.rodrigues@gmail.com>
2016-10-29 17:51:58 -0200

    Quase tudo OK

---
+++ JPA/workspace/Curso_Jpa/src/main/java/com/dextrainning/pessoa/PessoaService.java
@@ -0,0 +1,12 @@
+package com.dextrainning.pessoa;
+
+import com.dextrainning.service.GenericServices;
+
+public class PessoaService extends GenericServices<Pessoa> {
+
+	public PessoaService() {
+		super(Pessoa.class);
+		
+	}
+
+}
